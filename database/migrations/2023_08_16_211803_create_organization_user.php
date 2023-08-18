e935ebc803643a2b67cfba3d52e6541b3ddfdc6f
jan-eckerlein <eckerlein.jan@gmail.com>
2023-08-18 19:03:31 +0200

    fix delete Organization

--- database/migrations/2023_08_16_211803_create_organization_user.php
+++ database/migrations/2023_08_16_211803_create_organization_user.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('organization_user', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('organization_id')->constrained('organization');
-            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('organization_id')->constrained('organization')->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained('users')->cascadeOnDelete();
             $table->string('nickname')->nullable();
             $table->enum('org_role', ['owner', 'admin', 'member'])->default('member');
             $table->timestamps();
